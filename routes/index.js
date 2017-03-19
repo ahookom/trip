ba12e480c26039d520778b6eb99ac31f1189cc1b
Andrew Hookom <hookom@gmail.com>
2017-03-19 16:26:36 -0400

    'primitively styled search pages implemented'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 
 const router = require('express').Router();
-const models = require('../models');
+const db = require('../models');
+
+const Restaurants = db.models.restaurant;
+const Activities = db.models.activity;
+const Places = db.models.place;
+const Hotels = db.models.hotel;
 
 router.get('/', function(req, res, next) {
   res.render('index');
+})
+
+router.get('/restaurants/', function(req, res, next){
+  Restaurants.all().
+  then(function(restaurants){
+    res.render('restaurants', {restaurants});
+  }).
+  catch(next);
+})
+
+router.get('/hotels/', function(req, res, next){
+  Hotels.all().
+  then(function(hotels){
+    res.render('hotels', {hotels});
+  }).
+  catch(next);
+})
+
+router.get('/activities/', function(req, res, next){
+  Activities.all().
+  then(function(activities){
+    res.render('activities', {activities});
+  }).
+  catch(next);
 })
 
 module.exports = router;
